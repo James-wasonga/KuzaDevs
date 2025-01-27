6c930520cfc7073bf174c243ce8d99d04fa7ed21
James-wasonga <jameswasonga22@gmail.com>
2025-01-28 02:04:21 +0300

    remove commented code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -17,32 +17,3 @@ createRoot(document.getElementById('root')).render(
     </Router>
   </ThirdwebProvider>
 )
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import {BrowserRouter as Router} from 'react-router-dom';
-// import {ChainId, ThirdwebProvider} from "@thirdweb-dev/react";
-
-// // import { StateContextProvider } from './context';
-// import App from './App';
-// import './index.css';
-// //import AppProvider from './providers/AppProvider';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(
-  
-//    < ThirdwebProvider desiredChainId={ChainId.Goerli}>
-    
-//     <Router>
-//       <StateContextProvider>
-//         <App/>
-//       </StateContextProvider>
-//     </Router>
-    
-//    </ThirdwebProvider>
-  
-
-// )
-    
-
-
